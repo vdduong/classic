3cca71118be82e1caccde83cbe7bbab421f2c315
vdduong <vdduong@free.fr>
2013-10-23 10:20:05 +0200

    Update floyd_warshall.py

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -1,1 +1,42 @@
 # algorithm of Floyd Warshall to find all pairs of closest distance within a directed graph
+
+def adj(g):
+  """
+  convert a directed graph to an adjacency matrix
+  """
+  vertices = g.keys()
+  
+  dist = {}
+  for i in vertices:
+    dist[i] = {}
+    for j in vertices:
+      try:
+          dist[i][j] = g[i][j]
+      except KeyError:
+          # the distance from a node to itself is 0
+          if i == j:
+            dist[i][j] = 0
+          # the distance from a node to an unconnected node is infinity
+          else:
+            dist[i][j] = float('inf')
+  return dist
+  
+def fw(g):
+  """
+  run the Floyd Warshall algorithm on an adjacency matrix
+  The FW algorithm computes the minimum cost of a simple path between each pair of vertices
+  It's dynamic programming
+  """
+  
+  vertices = g.keys()
+  
+  d = dict(g)
+  for k in vertices:
+    for i in vertices:
+      for j in vertices:
+        d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+  return d
+  
+
+  
+  
