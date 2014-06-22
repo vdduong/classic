1126e6e86a202cb57ab3a10572c83f31252cf44b
vdduong <vdduong@free.fr>
2014-06-22 22:25:10 +0200

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,4 @@
+# the algorithm starts by building a table of neighbors so that for each vertex, it knows which other vertices it is connected to.
+# it then maintains two lists, a list of blacknodes (defined as vertices that have been visited) 
+# graynodes = vertices that have been discovered because the algorithm has visited its neighbor
+# return that blacknode list
