498eca34793ff02e07c3b27af39d00af9c977ce8
vdduong <vdduong@free.fr>
2014-06-22 22:57:55 +0200

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -2,3 +2,18 @@
 # it then maintains two lists, a list of blacknodes (defined as vertices that have been visited) 
 # graynodes = vertices that have been discovered because the algorithm has visited its neighbor
 # return that blacknode list
+
+def breadth_first_search(graph, start):
+  vertices, link = graph
+  blacknodes = []
+  graynodes = [start]
+  neighbors = [[] for vertex in vertices]
+  for link in links:
+    neighbors[link[0]].append(link[1])
+  while graynodes:
+    current = graynodes.pop()
+    for neighbor in neighbors[current]:
+      if not neighbor in blacknodes+graynodes:
+        graynodes.insert(0, neighbor)
+    blacknodes.append(current)
+  return blacknodes
