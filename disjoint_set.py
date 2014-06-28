8b7ad66b9ddeee6e2953e0576b7758578eb5ee72
vdduong <vdduong@free.fr>
2014-06-28 14:57:47 +0200

    Create disjoint_set.py

---
+++ disjoint_set.py
@@ -0,0 +1,5 @@
+# disjoint sets 
+# the set of sets is stored as an array of integers.
+# if at position i the array stores a negative number, this number is interpreted as being the representative element of its own set
+# if the number stored at position i is instead a nonnegative number j, it means that it belongs to a set that was joined with the set containing j
+
