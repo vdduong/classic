bc76dce97af967cb5d6a6de4dcf19c6a34214b82
vdduong <vdduong@free.fr>
2014-07-14 20:11:38 +0200

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,1 +1,16 @@
 # deep first search
+
+def depth_first_search(graph, start):
+  vertices, link = graph
+  blacknodes =[]
+  graynodes = [start]
+  neighbors = [[] for vertex in vertices]
+  for link in links:
+    neighbors[link[0]].append(link[1])
+  while graynodes:
+    current = graynodes.pop()
+    for neighbor in neighbors[current]:
+      if not neighbor in blacknodes+graynodes:
+        graynodes.append(neighbor)
+    blacknodes.append(current)
+  return blacknodes
