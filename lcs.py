8ae7b3fb9398c796ef5bd6f040fa0813579304c6
vdduong <vdduong@free.fr>
2014-07-22 17:00:30 +0200

    Update lcs.py

--- lcs.py
+++ lcs.py
@@ -6,4 +6,16 @@
 # here, we employ dynamic programming
 
 def lcs(a,b):
-  
+  previous = [0]*len(a)
+  for i,r in enumerate(a):
+    current = []
+    for j,c in enumerate(b):
+      if r==c:
+        e = previous[j-1]+1 if i*j>0 else 1
+      else:
+        e = max(previous[j] if i>0 else 0, current[-1] if j>0 else 0)
+      current.append(e)
+    previous=current
+  return current[-1]
+
+
