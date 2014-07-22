3a77f730e94d5c77397d12820de629bc7cb3fa0a
vdduong <vdduong@free.fr>
2014-07-22 16:53:02 +0200

    Update lcs.py

--- lcs.py
+++ lcs.py
@@ -1,1 +1,9 @@
 # Longest commun sequence
+# mutiple applications: molecular biology, spelling correction ..
+# three choices when the first characters differ:
+# remove from either string or remove from both
+# then again a recursive problem
+# here, we employ dynamic programming
+
+def lcs(a,b):
+  
