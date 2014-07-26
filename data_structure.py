7c63ae5591afc1fc818858f2d2ea888869578e0d
vdduong <vdduong@free.fr>
2014-07-27 00:36:19 +0200

    Create data_structure.py

---
+++ data_structure.py
@@ -0,0 +1,12 @@
+# data structures in Python
+
+# arrays
+# list: in Python, what is called a list is actually an array of pointers to the elements
+
+# stack
+# is a container, and it is usually implemented as a list. 
+" it has the property that the first thing you can take out is the last thing put in, commonly known as LIFO
+# the method to insert or add data to the container is called 'push'
+# and the method to extract data is called 'pop'
+
+stk =[]
